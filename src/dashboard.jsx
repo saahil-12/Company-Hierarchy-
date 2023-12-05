926da3c5e6abdc1c8c0a4d0a312832808b1452ae
saahil-12 <msaahilk12@gmail.com>
2023-12-05 18:37:07 +0530

    added tree also

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,28 +1,47 @@
+import React, { useEffect, useState } from "react";
 import "./dashboard.css"
+const TreeNode = ({ node }) => (
+  <div
+    style={{
+      marginLeft: "20px",
+      borderLeft: "1px solid #000",
+      paddingLeft: "10px",
+    }}
+  >
+    <br/>
+    <div>{`${node.name} - ${node.role}`}</div>
+    {node.children && node.children.length > 0 && (
+      <div>
+        {node.children.map((child, index) => (
+          <TreeNode key={index} node={child} />
+        ))}
+      </div>
+    )}
+  </div>
+);
 
-export default function Dashboard(){
-    return(
-        <div className="tree">
-            <ul>
-                <li><span>CEO</span>
-                    <ul>
-                        <li><span>CTO</span>
-                            <ul>
-                                <li><span>Product Engineer</span></li>
-                                <li><span>Buisness Analyst</span></li>
-                            </ul>
-                        </li>
-                        <li><span> Manager</span>
-                            <ul>
-                                <li><span>Developer</span></li>
-                                <li><span>Tester</span></li>
-                                <li><span>Designer</span></li>
-                            </ul>
-                        </li>
-                        <li><span> HR</span></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    );
-}
+const Tree = ({ data }) => (
+  <div>
+    {data.map((rootNode, index) => (
+      <TreeNode key={index} node={rootNode} />
+    ))}
+  </div>
+);
+
+const Dashboard = () => {
+const [data, setData] = useState([])
+  useEffect(() => {
+    let storedData = localStorage.getItem("formData");
+    storedData = storedData ? JSON.parse(storedData) : [];
+    setData(storedData[0].children)
+  }, []);
+
+  return (
+    <div>
+      <h1>Organization Hierarchy</h1>
+      <Tree data={data} />
+    </div>
+  );
+};
+
+export default Dashboard;
