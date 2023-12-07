ad5be4a57a4416680b1ae1d8f1ed7f0c4ae86969
muhammed saahil k <msaahilk12@gmail.com>
2023-12-08 01:22:52 +0530

    left layout set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "dark-purple":"#081A51",
+        'light-white':'rgba(255,255,255,0.18)'
+      }
+    },
   },
   plugins: [],
 }
